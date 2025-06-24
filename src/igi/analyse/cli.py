3981bd81ccefcd8c3eb81d6f1ee600fe23d28cbb
artiom.rotari <artiom.rotari@ebs-integrator.com>
2025-06-24 18:11:05 +0300

    Add more info about formats

--- src/igi/analyse/cli.py
+++ src/igi/analyse/cli.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 from pathlib import Path
 from zipfile import ZipFile
 
+from tabulate import tabulate
 from typer import Typer
 
 from ..config import Settings
@@ -9,8 +10,18 @@ from ..config import Settings
 app = Typer(name="igi_analyse", add_completion=False, short_help="Developer bootstrap tools")
 
 
+def print_formats(counter: defaultdict):
+    print(
+        tabulate(
+            tabular_data=sorted(counter.items(), key=lambda item: item[1], reverse=True),
+            headers=["Format", "Count"],
+            tablefmt="pipe",
+        )
+    )
+
+
 @app.command(short_help="List files by pattern")
-def game_dir_glob(pattern: str):
+def game_dir_glob(pattern: str, relative: bool = True):
     settings = Settings.load()
 
     if not settings.is_game_dir_configured():
@@ -18,24 +29,33 @@ def game_dir_glob(pattern: str):
 
     for number, path in enumerate(settings.game_dir.glob(pattern), start=1):
         if path.is_file():
-            print(f"[{number:>04}] {path.relative_to(settings.game_dir)}")
+            path_pretty = path.relative_to(settings.game_dir) if relative else path
+            print(f"[{number:>04}] {path_pretty.as_posix()}")
 
 
-@app.command(short_help="List extensions")
-def game_dir_extensions():
+@app.command(short_help="List formats")
+def game_dir_formats():
     settings = Settings.load()
 
     if not settings.is_game_dir_configured():
         return
 
-    extensions_counter = defaultdict(lambda: 0)
+    formats_counter = defaultdict(lambda: 0)
 
     for path in settings.game_dir.glob("**/*"):
-        if path.is_file():
-            extensions_counter[path.suffix] += 1
+        if not path.is_file():
+            continue
 
-    for extension, count in sorted(extensions_counter.items(), key=lambda item: item[1], reverse=True):
-        print(f"{extension}: {count}")
+        if path.suffix != ".dat":
+            format_name = f"`{path.suffix}`"
+        elif path.with_suffix(".mtp").exists():
+            format_name = "`.dat` (mtp)"
+        else:
+            format_name = "`.dat` (graph)"
+
+        formats_counter[format_name] += 1
+
+    print_formats(formats_counter)
 
 
 @app.command(short_help="List files by pattern")
@@ -66,29 +86,26 @@ def work_zip_list() -> None:
 
 
 @app.command(short_help="List extensions in zips")
-def work_zip_extensions(cumulative: bool = True) -> None:
+def work_zip_formats(cumulative: bool = True) -> None:
     settings = Settings.load()
 
     if not settings.is_work_dir_configured():
         return
 
-    extensions_counter = defaultdict(lambda: 0)
-    extensions_per_zip_counter = defaultdict(lambda: defaultdict(lambda: 0))
+    formats_counter = defaultdict(lambda: 0)
+    formats_per_zip_counter = defaultdict(lambda: defaultdict(lambda: 0))
 
     for number, path in enumerate(settings.work_dir.glob("**/*.zip"), start=1):
         with ZipFile(path) as zip_file:
             for sub_number, sub_name in enumerate(zip_file.namelist(), start=1):
-                extensions_counter[Path(sub_name).suffix] += 1
-                extensions_per_zip_counter[path.relative_to(settings.work_dir).as_posix()][Path(sub_name).suffix] += 1
+                format_name = f"`{Path(sub_name).suffix}`"
+                formats_counter[format_name] += 1
+                formats_per_zip_counter[path.relative_to(settings.work_dir).as_posix()][format_name] += 1
 
     if cumulative:
-        for extension, count in sorted(extensions_counter.items(), key=lambda item: item[1], reverse=True):
-            print(f"{extension} {count}")
+        print_formats(formats_counter)
     else:
-        for path, extensions_counter in extensions_per_zip_counter.items():
+        for path, formats_counter in formats_per_zip_counter.items():
             print(path)
-
-            for extension, count in sorted(extensions_counter.items(), key=lambda item: item[1], reverse=True):
-                print(f"{extension} {count}")
-
+            print_formats(formats_counter)
             print()
