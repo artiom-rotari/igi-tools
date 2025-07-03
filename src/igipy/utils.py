6ed84999e0766af823fdda3288edb58bf6fda7a5
artiom.rotari <artiom.rotari@ebs-integrator.com>
2025-07-03 13:28:21 +0300

    Fix ruff checks

--- src/igipy/utils.py
+++ src/igipy/utils.py
@@ -30,39 +30,44 @@ def validate_file_path_not_exists(path: Path) -> Path:
     return path
 
 
-def search_for_convert(
-    patterns: list[str],
-    src_dir: Path | None = None,
-    zip_dir: Path | None = None,
-) -> Generator[tuple[Path, Path, BytesIO]]:
-    if src_dir:
-        for src in src_dir.glob("**/*"):
-            for pattern in patterns:
-                if fnmatch(src.as_posix(), pattern):
-                    if src.is_file():
-                        src_path = src.relative_to(src_dir)
-                        src_stream = BytesIO(src.read_bytes())
+def search_for_convert_in_directory(patterns: list[str], directory: Path) -> Generator[tuple[Path, Path, BytesIO]]:
+    for src in directory.glob("**/*"):
+        for pattern in patterns:
+            if fnmatch(src.as_posix(), pattern) and src.is_file():
+                src_path = src.relative_to(directory)
+                src_stream = BytesIO(src.read_bytes())
+                yield src, src_path, src_stream
+
+
+def search_for_convert_in_zip(patterns: list[str], directory: Path) -> Generator[tuple[Path, Path, BytesIO]]:
+    for zip_path in directory.glob("**/*.zip"):
+        with zipfile.ZipFile(zip_path, "r") as zip_file:
+            for file_info in zip_file.infolist():
+                for pattern in patterns:
+                    if fnmatch(file_info.filename, pattern):
+                        src = zip_path.joinpath(file_info.filename)
+                        src_path = src.relative_to(directory)
+                        src_stream = BytesIO(zip_file.read(file_info))
                         yield src, src_path, src_stream
 
+
+def search_for_convert(
+    patterns: list[str], src_dir: Path | None = None, zip_dir: Path | None = None
+) -> Generator[tuple[Path, Path, BytesIO]]:
+    if src_dir:
+        yield from search_for_convert_in_directory(patterns=patterns, directory=src_dir)
+
     if zip_dir:
-        for zip_path in zip_dir.glob("**/*.zip"):
-            with zipfile.ZipFile(zip_path, "r") as zip_file:
-                for file_info in zip_file.infolist():
-                    for pattern in patterns:
-                        if fnmatch(file_info.filename, pattern):
-                            src = zip_path.joinpath(file_info.filename)
-                            src_path = src.relative_to(zip_dir)
-                            src_stream = BytesIO(zip_file.read(file_info))
-                            yield src, src_path, src_stream
+        yield from search_for_convert_in_zip(patterns=patterns, directory=zip_dir)
 
 
-def convert_all(
+def convert_all(  # noqa: PLR0913
     patterns: list[str],
     formater: type[formats.FileModel],
     dst_dir: Path,
     src_dir: Path | None = None,
     zip_dir: Path | None = None,
-    dry: bool = True,
+    dry: bool = True,  # noqa: FBT001, FBT002
 ) -> None:
     searcher = search_for_convert(patterns=patterns, src_dir=src_dir, zip_dir=zip_dir)
 
@@ -71,7 +76,8 @@ def convert_all(
         dst, dst_stream = formater.model_validate_stream(src_stream).model_dump_file(dst)
 
         typer.echo(
-            f'Convert [{i:>05}]: "{typer.style(src.as_posix(), fg="green")}" to "{typer.style(dst.as_posix(), fg="yellow")}"'
+            f'Convert [{i:>05}]: "{typer.style(src.as_posix(), fg="green")}" '
+            f'to "{typer.style(dst.as_posix(), fg="yellow")}"'
         )
 
         if not dry:
